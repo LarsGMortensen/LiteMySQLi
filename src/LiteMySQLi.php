ccae2f420feff4c28f1d8633463f918df4b88e76
Lars Mortensen <lars@grove-mortensen.dk>
2025-06-24 12:04:21 +0200

    Added namespace
    Add PSR-4 namespace declaration to LiteMySQLi class

--- src/LiteMySQLi.php
+++ src/LiteMySQLi.php
@@ -21,7 +21,11 @@
  * You should have received a copy of the GNU General Public License
  * along with LiteMySQLi. If not, see <https://www.gnu.org/licenses/>.
  */
- 
+
+
+namespace LiteMySQLi;
+
+
 class LiteMySQLi {
 	
 	/**
